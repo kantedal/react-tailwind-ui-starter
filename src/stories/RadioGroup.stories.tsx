ea5efc946776b8913dadaf1d2fd8b99d4df209a3
Marcus Svensson <marsven95@gmail.com>
2020-12-08 18:02:32 +0100

    changed import for tailwind

--- src/stories/RadioGroup.stories.tsx
+++ src/stories/RadioGroup.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../tailwind.output.css'
+import 'styles/main.css'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
 
